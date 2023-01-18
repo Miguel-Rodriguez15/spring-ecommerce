da65cb31b27fa87c0b752cae792970af786da445
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-01-18 15:22:39 -0500

    metodo listar, listado en la interface

--- src/main/java/com/spring/ecommerce/controller/ProductoController.java
+++ src/main/java/com/spring/ecommerce/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.spring.ecommerce.controller;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +12,22 @@ import com.spring.ecommerce.model.Producto;
 import com.spring.ecommerce.model.Usuario;
 import com.spring.ecommerce.service.ProductoService;
 
+
+
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);//variable para prueba
     
+	//creamos un objeto para poder acceder a los metodos crud de la clase
 	@Autowired
 	private ProductoService productoService;
 	
 	
 	@GetMapping("")
-	public String show() {
-	 return "productos/show";
+	public String show(Model model) {//este parametro es un objeto envia la lista de objeto producto hacia la vista show
+	 model.addAttribute("productos", productoService.findAll());
+		return "productos/show";
  }
       @GetMapping("create")
       public String create() {
@@ -32,9 +37,9 @@ public class ProductoController {
       @PostMapping("/save")
       public String save(Producto producto) {
     	  LOGGER.info("este es el objeto de producto {}",producto);//(asegurarnos de que tenga el metodo to string)
-    	Usuario u = new Usuario(1, "", "", "", "", "", "", "");
-    	producto.setUsuario(u);
-    	  productoService.save(producto);
+    	Usuario u = new Usuario(1, "", "", "", "", "", "", "");//llamo mi entidad usuario y sus respectivos atributos para enviar los datos por parametros
+    	producto.setUsuario(u);//almaceno los parametros enviados anteriormente
+    	  productoService.save(producto);//guardo mis parametros en la base de datos
     	  return "redirect:/productos";
       }
 }
--- src/main/java/com/spring/ecommerce/service/ProductoService.java
+++ src/main/java/com/spring/ecommerce/service/ProductoService.java
@@ -1,12 +1,14 @@
 package com.spring.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.spring.ecommerce.model.Producto;
 //creo mis metodos crud
 public interface ProductoService {
-   public Producto save(Producto producto);
-   public  Optional<Producto> get(Integer id);
-   public void update(Producto producto);
-   public void delete(Integer id);
+   public Producto save(Producto producto);//guardar
+   public  Optional<Producto> get(Integer id);//obtener un solo producto
+   public void update(Producto producto);//actualizar 
+   public void delete(Integer id);//eliminar
+   public List<Producto> findAll();//listar
 }
