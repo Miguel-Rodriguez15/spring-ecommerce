d3c13e66ff791294f7c4945525e319ac55565468
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-01-20 13:23:01 -0500

    metodo para mostrar la opcion ver producto y mostrar el id

--- src/main/java/com/spring/ecommerce/controller/HomeController.java
+++ src/main/java/com/spring/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.spring.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.ecommerce.service.ProductoService;
@@ -11,6 +14,7 @@ import com.spring.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")//le indico que se lanzara desde la ruta raiz
 public class HomeController {
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private ProductoService productoService;//lamo mis ervicios
 
@@ -18,5 +22,11 @@ public class HomeController {
 	public String home(Model model) {//llamo miclase model para poder llamar los atributos
 		model.addAttribute("productos", productoService.findAll());//traigo todos los atributos de productos
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id producto enviado como parametro{}",id);
+	return "usuario/productohome";
 	}
 }
