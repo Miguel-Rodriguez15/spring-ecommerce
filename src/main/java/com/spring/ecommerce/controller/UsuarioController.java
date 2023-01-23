7665d8821ff497fce73f549677bf38afd66f32c7
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-01-23 15:48:54 -0500

    testo de registro de usuario por consola
    verificación de que los datos se estén enviando al momento de realizar el registro

--- src/main/java/com/spring/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/spring/ecommerce/controller/UsuarioController.java
@@ -1,21 +1,32 @@
 package com.spring.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.ecommerce.model.Usuario;
 import com.spring.ecommerce.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-
+     private final Logger logger= LoggerFactory.getLogger(UsuarioController.class);
+	
 	@Autowired
 	private IUsuarioService usuarioService;
 	
 	@GetMapping("/registro")
 	public String create() {
 		return "usuario/registro";
+	}
+	
+	@PostMapping("/save")
+	public String save(Usuario usuario) {
+		logger.info("Usuario regustro:{}", usuario);
+		return "redirect:/";
 	}
 }
