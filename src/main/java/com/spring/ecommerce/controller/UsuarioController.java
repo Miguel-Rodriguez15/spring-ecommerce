e1dd9e0375145bf40fb9380293faf9845b1bd7c8
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-01-24 15:55:08 -0500

    Condición para mantener la sesión temporal del usuario

--- src/main/java/com/spring/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/spring/ecommerce/controller/UsuarioController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.spring.ecommerce.model.Usuario;
 import com.spring.ecommerce.service.IUsuarioService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -40,13 +42,31 @@ public class UsuarioController {
 		
 	}
 	@PostMapping("/acceder")
-	public String acceder(Usuario usuario) {
+	public String acceder(Usuario usuario, HttpSession session) {
 		//verificamos de que los datos estan siendo recibidos y los mostramos por consola
 		logger.info("accesos: {}", usuario);
-		//obtenemos un usuario por la interfaz y nos devuelbva uno igual a la base datos
+		//obtenemos un usuario por la interfaz y nos devuelva uno igual a la base datos
 		Optional<Usuario> user= usuarioService.findByEmail(usuario.getEmail());
 		/*regresamos por consola el registro del usuario con el correo que hemos enviado*/
-		logger.info("usuario de db: {}",user.get());
+		
+		/*en caso de que el usuario este presente
+		 * manetener la sesion de manera temporal,
+		 * si es ADMIN retornar a la vista del administrador,
+		 * si no es asi retornar  a la vista del usuario y si
+		 * no se encuentra el usuario presente se da por hecho que el 
+		 * usuario no existe*/
+		if(user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			if(user.get().getTipo().equals("ADMIN")) {
+				return"redirect:/administrador";
+			}else {
+				return"redirect:/";
+			}
+		}else {
+			logger.info("usuario no existe");
+		}
+		
+	
 		return "redirect:/";
 	}
 	
