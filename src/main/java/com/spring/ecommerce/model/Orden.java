e701e05e9ecbac33f9a54534f24921a4ab6fa060
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-01-24 20:02:24 -0500

    Correcion de relacionamiento
    probela generado por el mal relacionamiento generado en las clases modelos de orden y detalles

--- src/main/java/com/spring/ecommerce/model/Orden.java
+++ src/main/java/com/spring/ecommerce/model/Orden.java
@@ -1,98 +1,100 @@
 package com.spring.ecommerce.model;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
 @Entity
-@Table(name="ordenes")
+@Table(name = "ordenes")
 public class Orden {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id;
-private String numero;
-private Date FechaCreacion;
-private Date FechaRecibida;
-private double total;
+	private Integer id;
+	private String numero;
+	private Date FechaCreacion;
+	private Date FechaRecibida;
+	private double total;
 //Relacion de muchos auno
-@ManyToOne
-private Usuario usuario;
+	@ManyToOne
+	private Usuario usuario;
 
-@OneToOne(mappedBy = "orden")
-private DetalleOrden detalle;
-public Orden() {
-	// TODO Auto-generated constructor stub
-}
+	@OneToMany(mappedBy = "orden")
+	private List<DetalleOrden> detalle;
 
-public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
-	super();
-	this.id = id;
-	this.numero = numero;
-	FechaCreacion = fechaCreacion;
-	FechaRecibida = fechaRecibida;
-	this.total = total;
-	
-}
+	public Orden() {
+		// TODO Auto-generated constructor stub
+	}
 
-public Integer getId() {
-	return id;
-}
+	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		FechaCreacion = fechaCreacion;
+		FechaRecibida = fechaRecibida;
+		this.total = total;
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	}
 
-public String getNumero() {
-	return numero;
-}
+	public Integer getId() {
+		return id;
+	}
 
-public void setNumero(String numero) {
-	this.numero = numero;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public Date getFechaCreacion() {
-	return FechaCreacion;
-}
+	public String getNumero() {
+		return numero;
+	}
 
-public void setFechaCreacion(Date fechaCreacion) {
-	FechaCreacion = fechaCreacion;
-}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
 
-public Date getFechaRecibida() {
-	return FechaRecibida;
-}
+	public Date getFechaCreacion() {
+		return FechaCreacion;
+	}
 
-public void setFechaRecibida(Date fechaRecibida) {
-	FechaRecibida = fechaRecibida;
-}
+	public void setFechaCreacion(Date fechaCreacion) {
+		FechaCreacion = fechaCreacion;
+	}
 
-public double getTotal() {
-	return total;
-}
+	public Date getFechaRecibida() {
+		return FechaRecibida;
+	}
 
-public void setTotal(double total) {
-	this.total = total;
-}
+	public void setFechaRecibida(Date fechaRecibida) {
+		FechaRecibida = fechaRecibida;
+	}
 
+	public double getTotal() {
+		return total;
+	}
 
-public Usuario getUsuario() {
-	return usuario;
-}
+	public void setTotal(double total) {
+		this.total = total;
+	}
 
-public void setUsuario(Usuario usuario) {
-	this.usuario = usuario;
-}
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
-@Override
-public String toString() {
-	return "Orden [id=" + id + ", numero=" + numero + ", FechaCreacion=" + FechaCreacion + ", FechaRecibida="
-			+ FechaRecibida + ", total=" + total + "]";
-}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
+	@Override
+	public String toString() {
+		return "Orden [id=" + id + ", numero=" + numero + ", FechaCreacion=" + FechaCreacion + ", FechaRecibida="
+				+ FechaRecibida + ", total=" + total + "]";
+	}
 
 }
--- src/main/java/com/spring/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/spring/ecommerce/service/OrdenServiceImpl.java
@@ -18,38 +18,41 @@ public class OrdenServiceImpl implements IOrdenService {
 	@Override
 	public Orden save(Orden orden) {
 
-		return ordenRepository.save(orden);//obtenemos todas las ordenes
+		return ordenRepository.save(orden);// obtenemos todas las ordenes
 	}
 
 	@Override
 	public List<Orden> findAll() {
-		
+
 		return ordenRepository.findAll();
 	}
-	
+
 	public String generarNumeroOrden() {
-		int numero = 0;//con este vamos incrementando el numero de la ordes para despues pasarlo a un string
-		String numeroConcatenado="";//este nos va a devolver el numero en String
-		List<Orden> ordenes = findAll();//retornamos todas las ordenes
-		List<Integer> numeros=new ArrayList<Integer>();//iniciamos una lista como entero
-		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));//hacemops las convercion de cadenas de la base de datos a numeros
-		if(ordenes.isEmpty()) {
-			numero=1;
-		}else {//en caso de que no este vacia es uqe ya hay ordenes guardadas
-			numero=numeros.stream().max(Integer::compare).get();//pasamos el valor mayor de el objeto numero
-			numero++;//y que este objeto numero se  vaya incrementando
+		int numero = 0;// con este vamos incrementando el numero de la ordes para despues pasarlo a un
+						// string
+		String numeroConcatenado = "";// este nos va a devolver el numero en String
+		List<Orden> ordenes = findAll();// retornamos todas las ordenes
+		List<Integer> numeros = new ArrayList<Integer>();// iniciamos una lista como entero
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));// hacemops las convercion de
+																					// cadenas de la base de datos a
+																					// numeros
+		if (ordenes.isEmpty()) {
+			numero = 1;
+		} else {// en caso de que no este vacia es uqe ya hay ordenes guardadas
+			numero = numeros.stream().max(Integer::compare).get();// pasamos el valor mayor de el objeto numero
+			numero++;// y que este objeto numero se vaya incrementando
 		}
-		
-		if(numero<10) {// en caso dado el numero sea menor a 10 este se le agregaran 9 ceros
-			
-			numeroConcatenado="000000000"+String.valueOf(numero);
-		}else if(numero<100) {
-			numeroConcatenado="00000000"+String.valueOf(numero);
-		}else if(numero<1000) {
-			numeroConcatenado="0000000"+String.valueOf(numero);
-		}else if(numero<10000) {
-			numeroConcatenado="000000"+String.valueOf(numero);
+
+		if (numero < 10) { // 0000001000
+			numeroConcatenado = "000000000" + String.valueOf(numero);
+		} else if (numero < 100) {
+			numeroConcatenado = "00000000" + String.valueOf(numero);
+		} else if (numero < 1000) {
+			numeroConcatenado = "0000000" + String.valueOf(numero);
+		} else if (numero < 10000) {
+			numeroConcatenado = "0000000" + String.valueOf(numero);
 		}
+
 		return numeroConcatenado;
 	}
 
