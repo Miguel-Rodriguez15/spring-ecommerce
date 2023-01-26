18ef8f89eb41101f4be2ad1145e9cce3ade1bede
Miguel-Rodriguez15 <111886567+Miguel-Rodriguez15@users.noreply.github.com>
2023-01-26 15:58:12 -0500

    Creacion de la clase UserDetails

---
+++ src/main/java/com/spring/ecommerce/service/SpringBootSecurity.java
@@ -0,0 +1,14 @@
+package com.spring.ecommerce.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SpringBootSecurity {
+
+
+}
+
+
